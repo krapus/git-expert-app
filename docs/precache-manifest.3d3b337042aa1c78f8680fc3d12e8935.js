83467c44601d2a56b878d4ecac8c329234598350
Dario Sierra <dariosierra9@gmail.com>
2020-10-09 13:34:13 -0500

    docs agregado

---
+++ docs/precache-manifest.3d3b337042aa1c78f8680fc3d12e8935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a04513010f6ca7fc57e7f1e7dd0038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2384642e19649ac3276a",
+    "url": "/static/css/main.f8b7f537.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2384642e19649ac3276a",
+    "url": "/static/js/main.4462ec4c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
